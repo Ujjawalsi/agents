8b0fcf314997dabcff780698a580239eb5f4dfd1
ujjawal.singh <ujjawals97@gmail.com>
2023-09-27 16:33:56 +0530

    remove BullsEye from APIs

--- src/main/java/com/example/agents/constant/Constant.java
+++ src/main/java/com/example/agents/constant/Constant.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public class Constant {
     public static final String DOMAIN = "velocis.in";
     public static final String PROVIDER_URL = "ldap://192.168.100.240:389";
-    public static final String ldapUser="Asheesh Sharma";
-    public static final String ldapPassword="Prashant@2023";
+    public static final String ldapUser="bullseye";
+    public static final String ldapPassword="Bulls@2023";
 
 }
